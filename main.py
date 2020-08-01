a0419cf8abaeb65578bd07d8f51a1bf4c12d9604
Abhishek <abhishek.kumar@thoughts2binary.com>
2020-08-01 12:32:44 +0530

    commit-1 Telugu and english Ocr

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+from PIL import Image
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = dir_path + '/static/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img = Image.open('static/images/' + filename)
+            text = tess.image_to_string(img, lang='tel')
+            src = '../static/images/' + filename
+            return render_template('success.html', filename=filename, text=text, img=img, src=src)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
